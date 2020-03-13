737f58b0522bf909eb603b84caea1eb0f5ab35b8
stanlee321 <stanlee321@gmail.com>
2020-03-13 15:56:23 -0400

    updated code for handle antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {HelloComp} from './hello';
 import './App.css';
 
+import { Button } from 'antd';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi</h1>
         <HelloComp/>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
